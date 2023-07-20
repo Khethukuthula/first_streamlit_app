5f8f0b56fad79e007d11f7023edbdd56d07c485d
Khethukuthula <132578236+Khethukuthula@users.noreply.github.com>
2023-07-20 07:51:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.title('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.title('fruityvice fruit advice!')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit') 
@@ -22,6 +21,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 #requirements.txt
+streamlit.title('fruityvice fruit advice!')
 
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
