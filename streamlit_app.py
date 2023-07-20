0c6ebabda35590ddef16bd5e53682e25bb33edc5
Khethukuthula <132578236+Khethukuthula@users.noreply.github.com>
2023-07-20 08:44:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 #requirements.txt
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -31,5 +34,4 @@ streamlit.header("The fruit name contains:")
 streamlit.dataframe(my_data_rows)
 
 streamlit.write('Thanks for adding', add_my_fruit)
-
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
