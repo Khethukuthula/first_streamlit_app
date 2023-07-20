1f8390b37360614d73c96d8cfc879084e23660d5
Khethukuthula <132578236+Khethukuthula@users.noreply.github.com>
2023-07-20 07:50:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.title('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.title('fruityvice fruit advice!')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit') 
