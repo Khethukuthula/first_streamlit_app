43358a20400b7a7dbd4a8dd2feadb944272cb1d8
Khethukuthula <132578236+Khethukuthula@users.noreply.github.com>
2023-07-20 07:55:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 #requirements.txt
 streamlit.title('fruityvice fruit advice!')
+fruits_selected = streamlit.multiselect("What would you like information about:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
