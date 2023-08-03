e1470dde4d4d2166bc6d2040ba3ce5b6d95c006c
Khethukuthula <132578236+Khethukuthula@users.noreply.github.com>
2023-08-03 14:16:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,96 +1,31 @@
-import streamlit
-import pandas
-import requests
+import streamlit as st
 import snowflake.connector
-from urllib.error import URLError
 
- 
- 
- 
-streamlit.title('My Parents New Helathy Diner')
+# Streamlit app setup
+st.title("Upload Data to Snowflake")
 
- 
+# Snowflake connection setup
+conn = snowflake.connector.connect(
+    user='mhlongo',
+    password='Khetha370',
+    account='IU52295.ca-central-1.aws.snowflakecomputing.com',
+    warehouse='INTL_WH',
+    database='INTL.DB',
+    schema='PUBLIC',
+)
 
-streamlit.header('Breakfast Favorites')
-streamlit.text(' 🥣 Omega 3 & Bluebery Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+# Streamlit user interface
+data_to_upload = st.text_input("ISO_Countries_UTF8_pipe(1).csv")
+submit_button = st.button("Upload to Snowflake")
 
- 
+if submit_button and data_to_upload:
+    # Data processing and Snowflake insertion
+    cursor = conn.cursor()
+    try:
+        cursor.execute(f"INSERT INTO your_table_name (column1) VALUES ('{data_to_upload}')")
+        st.success("Data uploaded successfully!")
+    except Exception as e:
+        st.error(f"Error: {str(e)}")
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
- 
-
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
- 
-
-
-# Display the table on the page
-streamlit.dataframe(fruits_to_show)
-
- 
-
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = request.get("https://fruityvice.com/api/fruity/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
-
-#New Section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-   if not fruit_choice:
-        streamlit.error("Please select a fruit to get information.")
-   else:
-      back_from_function = get_fruityvice_data("kiwi")
-      streamlit.dataframe(back_from_function)
-except URLError as e:
-  streamlit.error()
-
- 
-
- 
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select* from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
- 
-
-
-def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('kiwi')")
-        return "Thanks for adding" + new_fruit
-
-
-
-
-
- 
-
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-if streamlit.button('Add a Fruit to the List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    back_from_function = insert_row_snowflake(add_my_fruit)
-    streamlit.text(back_from_function)
-
-def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor() as my_cur:
-        # Use a parameterized query with a placeholder (%s) for the new_fruit value
-        query = "INSERT INTO fruit_load_list (fruit_column_name) VALUES (%s)"
-        my_cur.execute(query, (new_fruit,))
-        my_cnx.commit()
-    return "Thanks for adding " + new_fruit
+# Close the Snowflake connection
+conn.close()
