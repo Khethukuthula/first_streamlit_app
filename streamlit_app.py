5c245e2b4133926331efbb2b70ab73f160eddfa2
Khethukuthula <132578236+Khethukuthula@users.noreply.github.com>
2023-08-29 16:45:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,3 @@
 import streamlit as st
 import snowflake.connector
 
-# Streamlit app setup
-st.title("Upload Data to Snowflake")
-
-# Snowflake connection setup
-conn = snowflake.connector.connect(
-    user='Mhlongo',
-    password='Khetha370',
-    account='IU52295.ca-central-1.aws.snowflakecomputing.com',
-    warehouse='INTL_WH',
-    database='INTL.DB',
-    schema='PUBLIC',
-)
-
-# Streamlit user interface
-data_to_upload = st.text_input("ISO_Countries_UTF8_pipe(1).csv:")
-submit_button = st.button("Upload to Snowflake")
-
-if submit_button and data_to_upload:
-    # Data processing and Snowflake insertion
-    cursor = conn.cursor()
-    try:
-        cursor.execute(f"INSERT INTO your_table_name (column1) VALUES ('{data_to_upload}')")
-        st.success("Data uploaded successfully!")
-    except Exception as e:
-        st.error(f"Error: {str(e)}")
-
-# Close the Snowflake connection
-conn.close()
